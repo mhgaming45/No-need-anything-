66edb27ea562e3a8ba0db9f7a8e373ad970c38b9
mhgaming45 <marotimdpbn@gmail.com>
2026-07-31 12:47:05 +0530

    Update testerpanel.js

--- commands/testerpanel.js
+++ commands/testerpanel.js
@@ -2,8 +2,6 @@ const {
   SlashCommandBuilder,
   EmbedBuilder,
   ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
   StringSelectMenuBuilder
 } = require("discord.js");
 
@@ -18,76 +16,68 @@ module.exports = {
       .setColor("Yellow")
       .setTitle("🎯 Tester Panel")
       .setDescription(
-`Welcome Tester!
+`Select a gamemode below and press **Next Player**.
 
-Use the button below to take the next player from queue.
-
-━━━━━━━━━━━━━━━━━━`
+━━━━━━━━━━━━━━━━━━━━`
       );
 
     const select = new StringSelectMenuBuilder()
-.setCustomId("select_test_mode")
-.setPlaceholder("🎮 Select Gamemode")
-.addOptions(
-  {
-    label: "Sword",
-    value: "sword",
-    emoji: "⚔️"
-  },
-  {
-    label: "Axe",
-    value: "axe",
-    emoji: "🪓"
-  },
-  {
-    label: "Crystal",
-    value: "crystal",
-    emoji: "💎"
-  },
-  {
-    label: "Mace",
-    value: "mace",
-    emoji: "🔨"
-  },
-  {
-    label: "UHC",
-    value: "uhc",
-    emoji: "🏹"
-  },
-  {
-    label: "SMP",
-    value: "smp",
-    emoji: "🌎"
-  },
-  {
-    label: "Netherite Pot",
-    value: "nethop",
-    emoji: "🔥"
-  },
-  {
-    label: "Diamond Pot",
-    value: "pot",
-    emoji: "💠"
-  },
-  {
-    label: "Cart",
-    value: "cart",
-    emoji: "🏎️"
+      .setCustomId("select_test_mode")
+      .setPlaceholder("🎮 Select Gamemode")
+      .addOptions(
+        {
+          label: "Sword",
+          value: "sword",
+          emoji: "⚔️"
+        },
+        {
+          label: "Axe",
+          value: "axe",
+          emoji: "🪓"
+        },
+        {
+          label: "UHC",
+          value: "uhc",
+          emoji: "🏹"
+        },
+        {
+          label: "Diamond Pot",
+          value: "pot",
+          emoji: "💎"
+        },
+        {
+          label: "Netherite Pot",
+          value: "nethop",
+          emoji: "🔥"
+        },
+        {
+          label: "SMP",
+          value: "smp",
+          emoji: "🌍"
+        },
+        {
+          label: "Mace",
+          value: "mace",
+          emoji: "🔨"
+        },
+        {
+          label: "Cart",
+          value: "cart",
+          emoji: "🏎️"
+        },
+        {
+          label: "Vanilla",
+          value: "vanilla",
+          emoji: "🌿"
+        }
+      );
+
+    const row = new ActionRowBuilder().addComponents(select);
+
+    await interaction.reply({
+      embeds: [embed],
+      components: [row]
+    });
+
   }
-);
-
-const row1 = new ActionRowBuilder()
-.addComponents(select);
-
-
-const row2 = new ActionRowBuilder()
-.addComponents(
-  new ButtonBuilder()
-    .setCustomId("next_player")
-    .setLabel("🎯 Next Player")
-    .setStyle(ButtonStyle.Primary)
-);
-await interaction.reply({
-  embeds: [embed],
-  components: [row1, row2]
-});
+};
