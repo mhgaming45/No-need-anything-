16176c1e1929a9a4669555086bd05a8afa0a6760
mhgaming45 <marotimdpbn@gmail.com>
2026-07-31 11:03:10 +0530

    Update testerpanel.js

--- commands/testerpanel.js
+++ commands/testerpanel.js
@@ -24,18 +24,65 @@ Use the button below to take the next player from queue.
 ━━━━━━━━━━━━━━━━━━`
       );
 
-    const row = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId("next_player")
-          .setLabel("🎯 Next Player")
-          .setStyle(ButtonStyle.Primary)
-      );
-
-    await interaction.reply({
-      embeds: [embed],
-      components: [row]
-    });
-
+    const select = new StringSelectMenuBuilder()
+.setCustomId("select_test_mode")
+.setPlaceholder("🎮 Select Gamemode")
+.addOptions(
+  {
+    label: "Sword",
+    value: "sword",
+    emoji: "⚔️"
+  },
+  {
+    label: "Axe",
+    value: "axe",
+    emoji: "🪓"
+  },
+  {
+    label: "Crystal",
+    value: "crystal",
+    emoji: "💎"
+  },
+  {
+    label: "Mace",
+    value: "mace",
+    emoji: "🔨"
+  },
+  {
+    label: "UHC",
+    value: "uhc",
+    emoji: "🏹"
+  },
+  {
+    label: "SMP",
+    value: "smp",
+    emoji: "🌎"
+  },
+  {
+    label: "Netherite Pot",
+    value: "nethop",
+    emoji: "🔥"
+  },
+  {
+    label: "Diamond Pot",
+    value: "pot",
+    emoji: "💠"
+  },
+  {
+    label: "Cart",
+    value: "cart",
+    emoji: "🏎️"
   }
-};
+);
+
+const row1 = new ActionRowBuilder()
+.addComponents(select);
+
+
+const row2 = new ActionRowBuilder()
+.addComponents(
+  new ButtonBuilder()
+    .setCustomId("next_player")
+    .setLabel("🎯 Next Player")
+    .setStyle(ButtonStyle.Primary)
+);
