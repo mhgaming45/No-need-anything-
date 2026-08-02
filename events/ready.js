829e0bbdd9602971f51116c5f73743bb2f3682b8
mhgaming45 <marotimdpbn@gmail.com>
2026-08-02 15:39:00 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,47 @@
-.
+const { Events, ActivityType } = require("discord.js");
+const updateQueue = require("../utils/updateQueue");
+const config = require("../config");
+
+module.exports = {
+
+    name: Events.ClientReady,
+
+    once: true,
+
+    async execute(client) {
+
+        console.log(`✅ Logged in as ${client.user.tag}`);
+
+        client.user.setPresence({
+
+            activities: [
+                {
+                    name: "Professional Tier Testing",
+                    type: ActivityType.Watching
+                }
+            ],
+
+            status: "online"
+
+        });
+
+        // Update all queue panels after restart
+        for (const gamemode of config.gamemodes) {
+
+            try {
+
+                await updateQueue(client, gamemode);
+
+            } catch (err) {
+
+                console.log(`Failed to update ${gamemode} queue.`);
+
+            }
+
+        }
+
+        console.log("✅ All Queue Panels Updated.");
+
+    }
+
+};
