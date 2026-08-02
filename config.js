db4a84cb60932057038c0caa3e01c874d896ccfa
mhgaming45 <marotimdpbn@gmail.com>
2026-08-02 10:06:00 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+    channels: {
+
+        register: "",
+
+        nethpot: "",
+        vanilla: "",
+        smp: "",
+        sword: "",
+        uhc: "",
+        mace: "",
+        axe: "",
+
+        logs: ""
+
+    },
+
+    roles: {
+
+        verified: "",
+
+        tester: "",
+        admin: "",
+
+        ht5: "",
+        ht4: "",
+        ht3: "",
+        ht2: "",
+        ht1: "",
+
+        lt1: "",
+        lt2: "",
+        lt3: "",
+        lt4: "",
+        lt5: ""
+
+    }
+
+};
