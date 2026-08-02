1f2c8b36001a7d808f7315af91fc90f78f5062d8
mhgaming45 <marotimdpbn@gmail.com>
2026-08-02 14:44:43 +0530

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,7 +1,19 @@
 const Database = require("better-sqlite3");
+const path = require("path");
 
-const db = new Database("./database/tierbot.db");
+// Database File
+const db = new Database(
+    path.join(__dirname, "database.sqlite")
+);
 
+// Better Performance
 db.pragma("journal_mode = WAL");
+db.pragma("foreign_keys = ON");
+db.pragma("synchronous = NORMAL");
+db.pragma("cache_size = 10000");
+db.pragma("temp_store = MEMORY");
+
+// Load Tables
+require("./schema");
 
 module.exports = db;
