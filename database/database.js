45af6231c7635ded0b7e255b07f9a472938b9b57
mhgaming45 <marotimdpbn@gmail.com>
2026-08-06 17:56:26 +0530

    Update database.js

--- database/database.js
+++ database/database.js
@@ -3,34 +3,19 @@ const path = require("path");
 
 const databasePath = path.join(__dirname, "database.json");
 
-// Create database if it doesn't exist
+// Create Database
 if (!fs.existsSync(databasePath)) {
-
-    const defaultData = {
-
-        players: {},
-
-        tiers: {},
-
-        queue: [],
-
-        active_tests: {},
-
-        queue_messages: {},
-
-        history: [],
-
-        matches: [],
-
-        testerStats: {},
-
-        settings: {}
-
-    };
 
     fs.writeFileSync(
         databasePath,
-        JSON.stringify(defaultData, null, 4)
+        JSON.stringify({
+            players: {},
+            queue: [],
+            active_tests: {},
+            queue_messages: {},
+            history: [],
+            testerStats: {}
+        }, null, 4)
     );
 
 }
@@ -38,9 +23,36 @@ if (!fs.existsSync(databasePath)) {
 // Load Database
 function load() {
 
-    return JSON.parse(
-        fs.readFileSync(databasePath, "utf8")
-    );
+    try {
+
+        const data = JSON.parse(
+            fs.readFileSync(databasePath, "utf8")
+        );
+
+        // Fix missing keys
+        if (!data.players) data.players = {};
+        if (!data.queue) data.queue = [];
+        if (!data.active_tests) data.active_tests = {};
+        if (!data.queue_messages) data.queue_messages = {};
+        if (!data.history) data.history = [];
+        if (!data.testerStats) data.testerStats = {};
+
+        return data;
+
+    } catch (err) {
+
+        console.error("Database Load Error:", err);
+
+        return {
+            players: {},
+            queue: [],
+            active_tests: {},
+            queue_messages: {},
+            history: [],
+            testerStats: {}
+        };
+
+    }
 
 }
 
@@ -54,9 +66,7 @@ function save(data) {
 
 }
 
-export default {
-
+module.exports = {
     load,
     save
-
 };
